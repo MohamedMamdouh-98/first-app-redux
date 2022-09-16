aca44c8b0c4e1993033c9e049a5465a1f76cb29c
MohamedMamdouh-98 <miido96@gmail.com>
2022-09-16 23:23:21 +0200

    [Add]componentIt

--- src/App.js
+++ src/App.js
@@ -1,43 +1,13 @@
-import { useSelector, useDispatch } from "react-redux";
 import "./App.css";
+import Counter from "./components/Counter";
+import { Provider } from "react-redux";
+import { store } from "./store";
 const App = () => {
-  //create dispatch
-  const dispatch = useDispatch();
-  //get state form store
-  const globalSelector = useSelector((state)=> state)
-  // const counterState = useSelector((state) => state.value);
-  // const toggleSelector = useSelector((state) => state.showCounter);
-
-  const increase = () => {
-    dispatch({ type: "increase", payload: 1 });
-  };
-
-  const decrease = () => {
-    dispatch({ type: "decrease", payload: 1 });
-  };
-const toggleShowCounter = ()=>{
-  dispatch({type: "hide-show"})
-}
   return (
     <div className="App">
-      <h1>Hello Redux Basic</h1>
-      {globalSelector.showCounter && (
-        <>
-          <div className="counter">Counter: {globalSelector.value}</div>
-          <div>
-            <button className="btn" onClick={increase}>
-              increase +
-            </button>
-            <button className="btn" onClick={decrease}>
-              decrease -
-            </button>
-          </div>
-        </>
-      )}
-
-      <div>
-        <button className="btn" onClick={toggleShowCounter}>Hide/Show Counter Number</button>
-      </div>
+      <Provider store={store}>
+        <Counter />
+      </Provider>
     </div>
   );
 };
